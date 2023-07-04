881dfe38b3c9a782ed03dd6058e8d27b915d623a
vinodpatil <vinod.patil377@gmail.com>
2023-07-04 10:36:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import { useState } from 'react';
+
 function App() {
+
+  let black = "black";
+  let green = "green"; 
+  const [bgcolor, setColor] = useState(black);
+
+  const ChangeBg = () =>{
+     const newBg = bgcolor === black ? green : black;
+     setColor(newBg);
+  }
+
+
+
+let cTime = new Date().toLocaleTimeString();
+const [state, setstate] = useState(cTime);
+
+ const ChangeTime = () =>{
+  cTime = new Date().toLocaleTimeString();
+   setstate(cTime)
+ }
+
+ setInterval(ChangeTime,1000);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <> 
+    <div className="container">
+        <div className="card" style={{backgroundColor : bgcolor}}>
+           <h1>{state}</h1>
+        </div>
+        <button className='btn' onClick={ChangeBg}>Change BgColor </button>
     </div>
+   </>
   );
 }
 
